756099533fb6d478b01530b5dcf7453f81671280
user <user@debian>
2016-07-15 18:10:02 +0300

    add features

--- app/main-page/main-page.component.js
+++ app/main-page/main-page.component.js
@@ -6,11 +6,27 @@ angular.
   component('mainPage', {
     templateUrl: 'main-page/main-page.template.html',
     controller: ['$http', '$routeParams',
-      function mainPageController($http, $routeParams) {
-      	this.lists = [{todoText:'Clean House'}, {todoText:'This is'}, {todoText:'Lock this'}];
+      function mainPageController() {
+      	this.lists = [{todoText:'Clean House'}];
+
+      	this.flag = false;
+
+      	this.placeholder = "Add new ToDo...";
 
       	this.addItem = function() {
-      		 this.lists.push({todoText:this.newListText});
+      		if(this.newListText=="" || this.newListText==undefined){this.flag = true;this.placeholder = "Hey, fill me!";}
+      		else{this.lists.push({todoText:this.newListText}); this.newListText=""; this.flag = false; this.placeholder = "Add new ToDo...";}
       	}
+
+      	this.removeItem = function(index) {
+      		this.lists.splice(index, 1);
+      	}
+
+      	this.editItem= function () {
+      		alert("Here will be an opportunity to correct the text.");
+      	}
+
+
+
       }]
   });
