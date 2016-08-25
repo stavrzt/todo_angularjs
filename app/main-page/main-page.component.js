d1b054d2a4cb84f12392d54c50c579a97d6bc25a
stavrzt <stavrzt@gmail.com>
2016-08-26 00:07:04 +0300

    add saving task progress

--- app/main-page/main-page.component.js
+++ app/main-page/main-page.component.js
@@ -11,28 +11,61 @@ angular.
 	  	try{this.lists = JSON.parse(localStorage["lists"]);}
         catch(err){this.lists = [];}
 
+        var update_values = function(){};
+
+
+        var uptodo = function(){
+          angular.element(document).ready(function () {
+          $(".save_progress").bind('keyup mouseup', function () {
+           var listss = JSON.parse(localStorage.lists);
+           var index = $(".save_progress").index(this);
+           listss[index].progress = Number(this.value);
+           localStorage.setItem("lists", JSON.stringify(listss));
+            });
+          });
+        }
+
       	this.placeholder = "Add new ToDo...";
 
       	this.addItem = function() {
+          this.lists = JSON.parse(localStorage.lists);
+          this.date = new Date();
       		if(this.newListText=="" || this.newListText==undefined){this.flag = true;this.placeholder = "Hey, fill me!";}
-      		else{this.lists.push({todoText:this.newListText, flag:false, dates:this.date}); localStorage.setItem("lists", JSON.stringify(this.lists)); this.newListText=""; this.flag = false; this.placeholder = "Add new ToDo..."; }	
+      		else{
+          this.lists.push({todoText:this.newListText, flag:false, dates:this.date, progress: 0}); 
+          localStorage.setItem("lists", JSON.stringify(this.lists));
+          this.newListText="";
+          this.flag = false; 
+          this.placeholder = "Add new ToDo..."; }
+
+          /*Функция обработки увеличения готовности таска СТАРТ*/
+          uptodo();
+          /*Функция обработки увеличения готовности таска КОНЕЦ*/
       	}
 
       	this.removeItem = function(index) {
+          this.lists = JSON.parse(localStorage.lists);
       		this.lists.splice(index, 1);
       		localStorage.setItem("lists", JSON.stringify(this.lists));
-      	}
 
-      	this.editItem = function () {
-      		alert("Here will be an opportunity to correct the text.");
-      	}
+          angular.element(document).ready(function () {
+          var array_progress = $(".save_progress");
+
+          for(var i=0; i<array_progress.length; i++){
+            array_progress[i].value = JSON.parse(localStorage.lists)[i].progress;
+          }
+      	});
+        }
 
       	this.flags = function(index, check) {
+          this.lists = JSON.parse(localStorage.lists);
       	  if(check){this.lists[index].flag = true; localStorage.setItem("lists", JSON.stringify(this.lists));}
       	  else{this.lists[index].flag = false; localStorage.setItem("lists", JSON.stringify(this.lists));}
       	} 
 
-
+        /*Функция обработки увеличения готовности таска СТАРТ*/
+        uptodo();
+        /*Функция обработки увеличения готовности таска КОНЕЦ*/
 
       }]
   });
