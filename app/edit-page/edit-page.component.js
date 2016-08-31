86979f86f5b7042f4437e8e530dc8e74ea87c2ec
stavrzt <stavrzt@gmail.com>
2016-08-31 20:59:34 +0300

    logic complete

--- app/edit-page/edit-page.component.js
+++ app/edit-page/edit-page.component.js
@@ -8,11 +8,16 @@ angular.
     controller: ['$http', '$routeParams',
       function editPageController($http, $routeParams) {
           this.lists = JSON.parse(localStorage["lists"]);
-          this.value = this.lists[$routeParams["todoid"]].todoText;
 
+          var position;
+          this.lists.forEach(function(for_item, i, arr) {
+            if(arr[i].id==$routeParams["todoid"]){position = i;}
+          });
+
+          this.value = this.lists[position].todoText;
 
           this.save = function(newText){
-          	this.lists[$routeParams["todoid"]].todoText = newText;
+          	this.lists[position].todoText = newText;
           	localStorage.setItem("lists", JSON.stringify(this.lists));
           }
       }]
--- app/main-page/main-page.component.js
+++ app/main-page/main-page.component.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Register `phoneDetail` component, along with its associated controller and template
 angular.
   module('mainPage').
   component('mainPage', {
@@ -8,64 +7,61 @@ angular.
     controller: ['$http', '$routeParams',
       function mainPageController() {
 
-	  	try{this.lists = JSON.parse(localStorage["lists"]);}
-        catch(err){this.lists = [];}
+        if(localStorage["lists"]==undefined){this.lists = [];}
+        else{this.lists = JSON.parse(localStorage["lists"]);}
 
-        var update_values = function(){};
+        this.placeholder = "Add new ToDo...";
+        this.ready = undefined;
 
-
-        var uptodo = function(){
-          angular.element(document).ready(function () {
-          $(".save_progress").bind('keyup mouseup', function () {
-           var listss = JSON.parse(localStorage.lists);
-           var index = $(".save_progress").index(this);
-           listss[index].progress = Number(this.value);
-           localStorage.setItem("lists", JSON.stringify(listss));
-            });
-          });
+        /*Функция изменения готовности таска++*/
+        this.uptodo = function(event, ids){
+          var itemPosition = this.returnPosition(ids);
+          if(event.target.valueAsNumber==100){this.flags(ids, true);}
+           this.lists[itemPosition].progress = event.target.valueAsNumber;
+           localStorage.setItem("lists", JSON.stringify(this.lists));
         }
-
-      	this.placeholder = "Add new ToDo...";
-
+        
+        /*Функция добавления таска*/
       	this.addItem = function() {
-          this.lists = JSON.parse(localStorage.lists);
+          if(localStorage["lists"]==undefined){this.lists = [];}
+          else{this.lists = JSON.parse(localStorage["lists"]);}
           this.date = new Date();
       		if(this.newListText=="" || this.newListText==undefined){this.flag = true;this.placeholder = "Hey, fill me!";}
-      		else{
-          this.lists.push({todoText:this.newListText, flag:false, dates:this.date, progress: 0}); 
+          else{
+            var newid;
+          if(this.lists.length==0){newid=0;}
+          else{newid=(this.lists[this.lists.length-1].id+1);}
+
+          this.lists.push({id:newid,todoText:this.newListText, flag:false, dates:this.date, progress: 0}); 
           localStorage.setItem("lists", JSON.stringify(this.lists));
           this.newListText="";
           this.flag = false; 
           this.placeholder = "Add new ToDo..."; }
-
-          /*Функция обработки увеличения готовности таска СТАРТ*/
-          uptodo();
-          /*Функция обработки увеличения готовности таска КОНЕЦ*/
       	}
 
-      	this.removeItem = function(index) {
-          this.lists = JSON.parse(localStorage.lists);
-      		this.lists.splice(index, 1);
-      		localStorage.setItem("lists", JSON.stringify(this.lists));
-
-          angular.element(document).ready(function () {
-          var array_progress = $(".save_progress");
-
-          for(var i=0; i<array_progress.length; i++){
-            array_progress[i].value = JSON.parse(localStorage.lists)[i].progress;
-          }
-      	});
+        /*Функция удаления таска*/
+      	this.removeItem = function(event, ids) {
+          var itemPosition = this.returnPosition(ids);
+          this.lists.splice(itemPosition, 1);
+          localStorage.setItem("lists", JSON.stringify(this.lists));
         }
 
-      	this.flags = function(index, check) {
-          this.lists = JSON.parse(localStorage.lists);
-      	  if(check){this.lists[index].flag = true; localStorage.setItem("lists", JSON.stringify(this.lists));}
-      	  else{this.lists[index].flag = false; localStorage.setItem("lists", JSON.stringify(this.lists));}
+        /*Функция отметки готовности таска*/
+      	this.flags = function(ids, check) {
+          var itemPosition = this.returnPosition(ids);
+      	  if(check){this.lists[itemPosition].flag = true;}
+      	  else{this.lists[itemPosition].flag = false;}
+          localStorage.setItem("lists", JSON.stringify(this.lists));
       	} 
 
-        /*Функция обработки увеличения готовности таска СТАРТ*/
-        uptodo();
-        /*Функция обработки увеличения готовности таска КОНЕЦ*/
+        this.returnPosition = function (ids) {
+          var position;
+          this.lists.forEach(function(for_item, i, arr) {
+            if(arr[i].id==ids){position = i;}
+          });
+          return position;
+        }
 
+        
       }]
   });
